dede09a44ef0ddefbf700bfe292c71c47539b4fc
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-03-16 22:12:38 +0100

    chore: update README, init tkinter
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # AiRope
 
 ## Description
-The idea is pretty simple, you have a locally (with an OpenAI, Claude, etc. support) installed AI application that
+The idea is pretty simple; you have a locally (with an OpenAI, Claude, etc. support) installed AI application that
 helps you with:
 1. **Work with documents**: save documents to the local database with tags, search, reminders, etc.
-2. **Work with code**: [TODO, but, connect the folder, sync changes with AI, get realtime suggestions (like testing for example)]
+2. **Work with code**: [TODO, but, connect the folder, sync changes with AI, get realtime suggestions (like testing, for example)]
 3. **RAG**, retrieval augment generation.
+4. **Desktop UI** based on Tkinter.
--- airope/maintk.py
+++ airope/maintk.py
@@ -4,8 +4,60 @@ import tkinter as tk
 
 async def run():
     root = tk.Tk()
-    label = tk.Label(root, text="Hello, world!")
-    label.pack()
+    root.title("Airope")
+    root.title = tk.Label(
+        root,
+        text="Welcome to the survey",
+        font=("Arial 16 bold", 16),
+        bg="brown",
+        fg="#FF0",
+    )
+    name_label: tk.Label = tk.Label(root, text="What is your name?")
+    name_inp: tk.Entry = tk.Entry(root)
+
+    eater_inp: tk.Checkbutton = tk.Checkbutton(
+        root,
+        text="Check this box if you eat bananas",
+    )
+
+    num_label: tk.Label = tk.Label(
+        root,
+        text="How many times do you eat bananas?",
+    )
+
+    num_inp: tk.Spinbox = tk.Spinbox(
+        root,
+        from_=0,
+        to=1000,
+        increment=1,
+    )
+
+    color_label: tk.Label = tk.Label(
+        root,
+        text="What is your favorite color?",
+    )
+
+    color_inp: tk.Listbox = tk.Listbox(root, height=1)
+    color_choices: tuple[str, ...] = (
+        "Any",
+        "Green",
+        "Green-Yellow",
+        "Yellow",
+        "Brown Spotted",
+        "Black",
+    )
+
+    for choice in color_choices:
+        color_inp.insert(tk.END, choice)
+
+    root.geometry("640x480+300+300")
+    root.resizable(False, False)
+
+    plantain_l = tk.Label(root, text="Do you like plantains?")
+    plantain_f = tk.Frame(root)
+    plantain_y_inp = tk.Radiobutton(plantain_l, text="Yes")
+    plantain_n_inp = tk.Radiobutton(plantain_l, text="No")
+
     root.mainloop()
 
 
