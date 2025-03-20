5f73c6a9656f76b1b27cae8b42fd3b9a08af5e01
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-03-20 19:12:03 +0100

    chore: switch to pyside
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- airope/maintk.py
+++ airope/maintk.py
@@ -1,64 +1,23 @@
 import asyncio
-import tkinter as tk
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from frontend.ui_mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
 async def run():
-    root = tk.Tk()
-    root.title("Airope")
-    root.title = tk.Label(
-        root,
-        text="Welcome to the survey",
-        font=("Arial 16 bold", 16),
-        bg="brown",
-        fg="#FF0",
-    )
-    name_label: tk.Label = tk.Label(root, text="What is your name?")
-    name_inp: tk.Entry = tk.Entry(root)
-
-    eater_inp: tk.Checkbutton = tk.Checkbutton(
-        root,
-        text="Check this box if you eat bananas",
-    )
-
-    num_label: tk.Label = tk.Label(
-        root,
-        text="How many times do you eat bananas?",
-    )
-
-    num_inp: tk.Spinbox = tk.Spinbox(
-        root,
-        from_=0,
-        to=1000,
-        increment=1,
-    )
-
-    color_label: tk.Label = tk.Label(
-        root,
-        text="What is your favorite color?",
-    )
-
-    color_inp: tk.Listbox = tk.Listbox(root, height=1)
-    color_choices: tuple[str, ...] = (
-        "Any",
-        "Green",
-        "Green-Yellow",
-        "Yellow",
-        "Brown Spotted",
-        "Black",
-    )
-
-    for choice in color_choices:
-        color_inp.insert(tk.END, choice)
-
-    root.geometry("640x480+300+300")
-    root.resizable(False, False)
-
-    plantain_l = tk.Label(root, text="Do you like plantains?")
-    plantain_f = tk.Frame(root)
-    plantain_y_inp = tk.Radiobutton(plantain_l, text="Yes")
-    plantain_n_inp = tk.Radiobutton(plantain_l, text="No")
-
-    root.mainloop()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
